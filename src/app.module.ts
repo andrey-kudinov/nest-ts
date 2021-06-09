cd0c28323eff70226881e7450b7abfda45f34b2d
Andrey <2499931@gmail.com>
2021-06-09 11:23:47 +0400

    добавил env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    TopPageModule,
+    ProductModule,
+    ReviewModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
